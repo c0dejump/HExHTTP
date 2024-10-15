38a61e03c149e9ed952a8c124e7d2836046a8716
Kharaone.BB <bugbounty@khara.one>
2024-10-15 16:03:51 +0200

    refactor: streamline output and remove unnecessary function
    - DRY the output display
    - Removed compareRequests function as it was just doing a get request

--- modules/cpdos/hbh.py
+++ modules/cpdos/hbh.py
@@ -10,28 +10,21 @@ from ..utils import *
 
 VULN_NAME = "Hop-By-Hop"
 
-# Function to make requests and compare responses
-def compareRequests(url, s, headers, resp1, params2, authent):
-    try:
-        resp2 = s.get(url, headers=headers, params=params2, auth=authent, allow_redirects=False, verify=False, timeout=10)
-        return resp2
-    except requests.exceptions.ConnectionError as e:
-        #print(f"Error : {e}")
-        return None
-
-
 # Function to test for cache poisoning
-def CachePoisoning(url, s, params2, resp1, resp2, authent, headers):
+def cachePoisoning(url, s, params2, resp1, resp2, authent, headers):
     try:
         resp3 = s.get(url, params=params2, auth=authent, allow_redirects=False, verify=False, timeout=10)
     except requests.exceptions.ConnectionError as e:
         pass
         #print(f"Error : {e}")
-    
+    reason = ""
     if resp3.status_code == resp2.status_code and resp3.status_code != resp1.status_code and resp3.status_code != 429:
-        print(f" \033[31m└── [VULNERABILITY CONFIRMED]\033[0m | {VULN_NAME} | \033[34m{url}?cacheBuster={params2['cacheBuster']}\033[0m | DIFFERENT STATUS-CODE {resp1.status_code} > {resp3.status_code} | PAYLOAD: Connection: {headers['Connection']}")
+        reason = f"DIFFERENT STATUS-CODE {resp1.status_code} > {resp3.status_code}"
     if len(resp3.content) == len(resp2.content) and len(resp3.content) != len(resp1.content) and resp3.status_code != 429:
-        print(f" \033[31m└── [VULNERABILITY CONFIRMED]\033[0m | {VULN_NAME} | \033[34m{url}?cacheBuster={params2['cacheBuster']}\033[0m | DIFFERENT RESPONSE LENGTH {len(resp1.content)}b > {len(resp3.content)}b | PAYLOAD: Connection: {headers['Connection']}")
+        reason = f"DIFFERENT RESPONSE LENGTH {len(resp1.content)}b > {len(resp3.content)}b"
+
+    if reason:
+        print(f" \033[31m└── [VULNERABILITY CONFIRMED]\033[0m | {VULN_NAME} | \033[34m{url}?cacheBuster={params2['cacheBuster']}\033[0m | {reason} | PAYLOAD: Connection: {headers['Connection']}")
 
 
 def HBH(url, s, req_main, main_len, main_status_code, authent):
@@ -50,7 +43,7 @@ def HBH(url, s, req_main, main_len, main_status_code, authent):
             headers = {'Connection': f'keep-alive, {header}'}
             params2 = {'cacheBuster': generate_cache_buster()}
             try:
-                resp2 = compareRequests(url, s, headers, resp1, params2, authent)
+                resp2 = s.get(url, headers=headers, params=params2, auth=authent, allow_redirects=False, verify=False, timeout=10)
                 #print(f"return: {resp2}") #DEBUG
                 #print(resp2_stat) #DEBUG
 
@@ -66,16 +59,19 @@ def HBH(url, s, req_main, main_len, main_status_code, authent):
                 else:
                     resp2_count_size += 1
 
+                behavior = ""
                 if resp1.status_code != resp2.status_code and resp2.status_code not in [429, 403] and resp1.status_code not in [301, 302, 429, 403] and resp2_count_code < 3:
-                    behavior = "DIFFERENT STATUS-CODE"
-                    print(f" \033[33m└── [INTERESTING BEHAVIOR]\033[0m | {VULN_NAME} | \033[34m{url}?cacheBuster={params2['cacheBuster']}\033[0m | {behavior} {resp1.status_code} > {resp2.status_code} | PAYLOAD: Connection: {headers['Connection']}")
-                    CachePoisoning(url, s, params2, resp1, resp2, authent, headers)
+                    behavior = f"DIFFERENT STATUS-CODE  {resp1.status_code} > {resp2.status_code}"
+
                 if len(resp1.content) not in range(len(resp2.content) - 1000, len(resp2.content) + 1000) and resp2.status_code not in [429, 403] and resp1.status_code not in [301, 302, 429, 403] and resp2_count_size < 3:
-                    behavior = "DIFFERENT RESPONSE LENGTH"
-                    print(f" \033[33m└── [INTERESTING BEHAVIOR]\033[0m | {VULN_NAME} | \033[34m{url}?cacheBuster={params2['cacheBuster']}\033[0m | {behavior} {len(resp1.content)}b > {len(resp2.content)}b | PAYLOAD: Connection: {headers['Connection']}")
-                    CachePoisoning(url, s, params2, resp1, resp2, authent, headers)
+                    behavior = f"DIFFERENT RESPONSE LENGTH  {len(resp1.content)}b > {len(resp2.content)}b"
+
+                if behavior:
+                    print(f" \033[33m└── [INTERESTING BEHAVIOR]\033[0m | {VULN_NAME} | \033[34m{url}?cacheBuster={params2['cacheBuster']}\033[0m | {behavior} | PAYLOAD: Connection: {headers['Connection']}")
+                    cachePoisoning(url, s, params2, resp1, resp2, authent, headers)
+
             except Exception as e:
                 pass
                 #print(f"Error : {e}")
-            sys.stdout.write(f"\033[34m {headers}\033[0m\r")
+            sys.stdout.write(f" \033[34m {headers}\033[0m\r")
             sys.stdout.write("\033[K")
