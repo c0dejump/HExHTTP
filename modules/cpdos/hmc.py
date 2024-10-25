75bbe033d327cf3036bc359b711d1e431b668311
Kharaone.BB <bugbounty@khara.one>
2024-10-25 02:37:36 +0200

    refactor(hmc): refactored and linted HMC module
    - Linting applied
    - Kept function name
    - added a few metacharacters
    - set up of the logging

--- modules/cpdos/hmc.py
+++ modules/cpdos/hmc.py
@@ -6,17 +6,61 @@ Attempts to find Cache Poisoning with HTTP Metachar Character (HMC)
 https://cpdos.org/#HMC
 """
 
-from ..utils import *
+from modules.utils import logging, random
 
-VULN_NAME = "HTTP Metachar Character"
+VULN_NAME = "HTTP Meta Character"
+
+logger = logging.getLogger(__name__)
+
+def check_meta_character(url, s, main_status_code, authent, meta_character):
+    """Probe and Verify the server for a meta character vulnerability"""
+
+    url = f"{url}{random.randrange(99)}"
+    headers = {"X-Metachar-Header": meta_character}
+    probe = s.get(
+        url,
+        headers=headers,
+        timeout=10,
+        verify=False,
+        auth=authent,
+        allow_redirects=False,
+    )
+
+    reason = ""
+    if (
+        probe.status_code in [400, 413, 500]
+        and probe.status_code != main_status_code
+    ):
+        control = s.get(url, verify=False, timeout=10, auth=authent)
+        if (
+            control.status_code == probe.status_code
+            and control.status_code != main_status_code
+        ):
+            reason = f"\033[34m{main_status_code} > {control.status_code}\033[0m"
+
+    if reason:
+        payload = f"PAYLOAD: {headers}"
+        print(
+            f"\033[31m └── [VULNERABILITY CONFIRMED]\033[0m | HMC | {url} | {reason} | {payload}"
+        )
 
 def HMC(url, s, main_status_code, authent):
-    chars = [r"\n", r"\a", r"\r", "\x07\x07\x07\x07\x07\x07\x07\x07\x07\x07\x07\x07\x07\x07\x07\x07metahttptest"]
-    for c in chars:
-        url = f"{url}{random.randrange(99)}"
-        headers = {"X-Metachar-Header": c}
-        req_hmc = s.get(url, headers=headers, timeout=10, verify=False, auth=authent, allow_redirects=False)
-        if req_hmc.status_code in [400, 413, 500] and req_hmc.status_code != main_status_code:
-            req_verify_hmc = s.get(url, verify=False, timeout=10, auth=authent)
-            if req_verify_hmc.status_code == req_hmc.status_code and req_verify_hmc.status_code != main_status_code:
-                print("  \033[31m └── [VULNERABILITY CONFIRMED]\033[0m | HMC DOS: {} | \033[34m{} > {}\033[0m | PAYLOAD: {}".format(url, main_status_code, req_verify_hmc.status_code, headers))
+    """Prepare the list of meta characters to check for"""
+
+    meta_characters = [
+        r"\n",
+        r"\a",
+        r"\r",
+        r"\0",
+        r"\b",
+        r"\e",
+        r"\v",
+        r"\f",
+        r"\u0000",
+        "\x07\x07\x07\x07\x07\x07\x07\x07\x07\x07\x07\x07\x07\x07\x07\x07metahttptest",
+    ]
+    for meta_character in meta_characters:
+        check_meta_character(url, s, main_status_code, authent, meta_character)
+
+        print(f" \033[34m {VULN_NAME} : {meta_character.encode(encoding='UTF-8')}\033[0m\r", end="")
+        print("\033[K", end="")
