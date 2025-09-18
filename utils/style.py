6f426b66cf8944618b4307ec9e368183e4e95520
Kharaone.BB <bugbounty@khara.one>
2025-09-18 17:04:24 +0200

    lint(style): ordering colors for faster lookup

--- utils/style.py
+++ utils/style.py
@@ -1,11 +1,11 @@
 class Colors:
     """Colors constants for the output messages"""
     RED = "\033[31m"
-    YELLOW = "\033[33m"
     GREEN = "\033[32m"
+    YELLOW = "\033[33m"
     BLUE = "\033[34m"
-    CYAN = "\033[36m"
     MAGENTA = "\033[35m"
+    CYAN = "\033[36m"
     SALMON = "\033[38;2;255;174;157m"
     THISTLE = "\033[38;2;197;197;169m"
     REDIR = "\033[38;2;245;203;92m"
