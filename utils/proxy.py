e50f820c0a49287df0a195f6975821d5ae686975
Kharaone.BB <bugbounty@khara.one>
2025-09-20 19:08:52 +0200

    refactor(proxy): improve code readability by formatting function parameters and updating import statement

--- utils/proxy.py
+++ utils/proxy.py
@@ -2,11 +2,12 @@
 
 import json
 
-import requests
+from utils.utils import requests
 
 proxy_enabled = False
 
 proxy_url = "http://127.0.0.1:8080"
+
 
 def create_burp_issue(
     s: requests.Session,
@@ -14,20 +15,18 @@ def create_burp_issue(
     title: str,
     description: str,
     severity: str,
-    headers: dict
+    headers: dict,
 ) -> bool:
-    try:        
-        issue_data = json.dumps({
-            "title": title,
-            "description": description,
-            "severity": severity
-        })
-        
+    try:
+        issue_data = json.dumps(
+            {"title": title, "description": description, "severity": severity}
+        )
+
         headers.update({"X-Create-Burp-Issue": issue_data})
-        
+
         s.get(url, headers=headers, timeout=10)
         return True
-        
+
     except Exception as e:
         print(f"Error: {e}")
         return False
@@ -37,16 +36,17 @@ def proxy_request(
     s: requests.Session,
     url: str,
     method: str,
-    headers: dict[str,str] = dict(),
+    headers: dict[str, str] = dict(),
     data: str | None = None,
-    severity: str = ""
+    severity: str = "",
 ) -> None:
 
-    headers.update({"User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:139.0) Gecko/20100101 Firefox/139.0"})
-    s.proxies = {
-            "http": proxy_url,
-            "https": proxy_url
-    }
+    headers.update(
+        {
+            "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:139.0) Gecko/20100101 Firefox/139.0"
+        }
+    )
+    s.proxies = {"http": proxy_url, "https": proxy_url}
 
     s.verify = False
 
@@ -56,19 +56,19 @@ def proxy_request(
             create_burp_issue(
                 s,
                 url,
-                "[HExHTTP] Behavior", 
+                "[HExHTTP] Behavior",
                 f"Cache poisoning vulnerability detected on {url}",
                 "Medium",
-                headers
+                headers,
             )
         elif severity == "confirmed":
             create_burp_issue(
                 s,
                 url,
-                "[HExHTTP] Confirmed", 
+                "[HExHTTP] Confirmed",
                 f"Cache poisoning vulnerability detected on {url}",
                 "High",
-                headers
+                headers,
             )
     except Exception as e:
         print(f"Error : {e}")
