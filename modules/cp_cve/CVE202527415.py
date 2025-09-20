a1ee510c7d16dd05e68fb593f4f7350fb98e67dd
Kharaone.BB <bugbounty@khara.one>
2025-09-20 15:16:49 +0200

    lint: enhance code readability and consistency in nuxt_check function

--- modules/cp_cve/CVE202527415.py
+++ modules/cp_cve/CVE202527415.py
@@ -6,10 +6,11 @@ https://zhero-web-sec.github.io/research-and-things/nuxt-show-me-your-payload
 """
 
 from modules.cp_cve.unrisk_page import get_unrisk_page
-from utils.style import Identify
+from utils.style import Colors, Identify
 from utils.utils import configure_logger, requests, sys
 
 logger = configure_logger(__name__)
+
 
 def nuxt_check(
     url: str,
@@ -19,47 +20,82 @@ def nuxt_check(
     authent: tuple[str, str] | None,
 ) -> None:
     try:
-        req = requests.get(url, verify=False, auth=authent, headers=custom_header, timeout=10, allow_redirects=False)
+        req = s.get(
+            url,
+            verify=False,
+            auth=authent,
+            headers=custom_header,
+            timeout=10,
+            allow_redirects=False,
+        )
         unrisk_page = get_unrisk_page(url, req_main)
         if unrisk_page:
-            poison_url = f"{unrisk_page}_payload.json" if unrisk_page[-1] == "/" else f"{unrisk_page}/_payload.json"
+            poison_url = (
+                f"{unrisk_page}_payload.json"
+                if unrisk_page[-1] == "/"
+                else f"{unrisk_page}/_payload.json"
+            )
             try:
-                req_nuxt = requests.get(poison_url, verify=False, auth=authent, headers=custom_header, timeout=10, allow_redirects=False)
+                req_nuxt = s.get(
+                    poison_url,
+                    verify=False,
+                    auth=authent,
+                    headers=custom_header,
+                    timeout=10,
+                    allow_redirects=False,
+                )
                 try:
                     req_nuxt.json()
-                    print(f" {Identify.behavior} | CVE-2025-27415 | TAG OK | \033[34m{poison_url}\033[0m")
+                    print(
+                        f" {Identify.behavior} | CVE-2025-27415 | TAG OK | {Colors.BLUE}{poison_url}{Colors.RESET}"
+                    )
                 except requests.exceptions.JSONDecodeError:
                     if "application/json" in req_nuxt.headers.get("Content-Type", ""):
-                        print(f" {Identify.behavior} | CVE-2025-27415 | TAG OK | \033[34m{poison_url}\033[0m")
+                        print(
+                            f" {Identify.behavior} | CVE-2025-27415 | TAG OK | {Colors.BLUE}{poison_url}{Colors.RESET}"
+                        )
                     if req_nuxt.status_code != req.status_code:
-                        print(f" {Identify.behavior} | CVE-2025-27415 | DIFFERENT RESPONSE {req.status_code} > {req_nuxt.status_code}| \033[34m{url}\033[0m")
+                        print(
+                            f" {Identify.behavior} | CVE-2025-27415 | DIFFERENT RESPONSE {req.status_code} > {req_nuxt.status_code}| {Colors.BLUE}{url}{Colors.RESET}"
+                        )
                 except Exception as e:
                     logger.exception(e)
-                    pass
                 # Check exploit
-                req_verify = requests.get(unrisk_page, verify=False, auth=authent, headers=custom_header, timeout=10, allow_redirects=False)
+                req_verify = s.get(
+                    unrisk_page,
+                    verify=False,
+                    auth=authent,
+                    headers=custom_header,
+                    timeout=10,
+                    allow_redirects=False,
+                )
                 try:
                     req_verify.json()
-                    print(f" {Identify.confirmed} | CVE-2025-27415 | TAG OK | \033[34m{unrisk_page}\033[0m")
+                    print(
+                        f" {Identify.confirmed} | CVE-2025-27415 | TAG OK | {Colors.BLUE}{unrisk_page}{Colors.RESET}"
+                    )
                 except requests.exceptions.JSONDecodeError:
                     if "application/json" in req_verify.headers.get("Content-Type", ""):
-                        print(f" {Identify.confirmed} | CVE-2025-27415 | TAG OK | \033[34m{unrisk_page}\033[0m")
+                        print(
+                            f" {Identify.confirmed} | CVE-2025-27415 | TAG OK | {Colors.BLUE}{unrisk_page}{Colors.RESET}"
+                        )
                     if req_verify.status_code != req.status_code:
-                        print(f" {Identify.confirmed} | CVE-2025-27415 | DIFFERENT RESPONSE {req.status_code} > {req_verify.status_code} | \033[34m{unrisk_page}\033[0m")
+                        print(
+                            f" {Identify.confirmed} | CVE-2025-27415 | DIFFERENT RESPONSE {req.status_code} > {req_verify.status_code} | {Colors.BLUE}{unrisk_page}{Colors.RESET}"
+                        )
                 except Exception as e:
                     logger.exception(e)
-                    pass
             except Exception as e:
                 logger.exception(e)
-                pass
         else:
-            print(" CVE-2025-27415 | [i] It seems that the nuxt.js framework is used, but no risk-free pages have been found. Please do a manual check.")
+            print(
+                " CVE-2025-27415 | [i] It seems that the nuxt.js framework is used, but no risk-free pages have been found. Please do a manual check."
+            )
 
-    except requests.Timeout:
-        pass
+    except requests.Timeout as t:
+        logger.error("request timeout", t)
     except KeyboardInterrupt:
         print("Exiting")
         sys.exit()
     except Exception as e:
         logger.exception(e)
-        pass
