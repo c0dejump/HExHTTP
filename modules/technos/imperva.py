5ef7fcc4a998cdd40b69af4b449e5d946c012c4c
Kharaone.BB <bugbounty@khara.one>
2025-09-20 18:41:47 +0200

    refactor(imperva): improve code readability by formatting imports and docstring

--- modules/technos/imperva.py
+++ modules/technos/imperva.py
@@ -1,22 +1,25 @@
 #!/usr/bin/env python3
 
-from utils.utils import requests
+from utils.utils import configure_logger, requests
+
+logger = configure_logger(__name__)
 
 
 def imperva(url: str, s: requests.Session) -> None:
     """
-      https://docs.imperva.com/bundle/cloud-application-security/page/settings/xray-debug-headers.htm
-      https://docs.imperva.com/bundle/advanced-bot-protection/page/74736.htm
-      incap-cache-key
-      incap-cache-reason
-      x-distil-debug
+    https://docs.imperva.com/bundle/cloud-application-security/page/settings/xray-debug-headers.htm
+    https://docs.imperva.com/bundle/advanced-bot-protection/page/74736.htm
+    incap-cache-key
+    incap-cache-reason
+    x-distil-debug
     """
     imperva_list = ["incap-cache-key", "incap-cache-reason", "x-distil-debug"]
     for il in imperva_list:
         try:
             headers = {il: "1"}
             req = s.get(url, headers=headers, verify=False, timeout=10)
-            print(f"   └── {il}{'→':^3} {req.status_code:>3} [{len(req.content)} bytes]")
-        except Exception:
-            pass
-    
+            print(
+                f"   └── {il}{'→':^3} {req.status_code:>3} [{len(req.content)} bytes]"
+            )
+        except Exception as e:
+            logger.exception(f"Error with Imperva check on {url}", e)
