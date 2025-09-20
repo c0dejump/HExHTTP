8830c5688d256e7aef610cbeec41591f1491df9d
Kharaone.BB <bugbounty@khara.one>
2025-09-20 16:06:26 +0200

    lint: improve code readability by formatting function definitions and conditional statements in multiple_headers.py

--- modules/cpdos/multiple_headers.py
+++ modules/cpdos/multiple_headers.py
@@ -1,21 +1,31 @@
 import http.client
-from urllib.parse import urlparse
 
 from modules.lists import header_list
 from utils.style import Colors, Identify
-from utils.utils import configure_logger, human_time, random, requests
+from utils.utils import configure_logger, human_time, random, requests, urlparse
 
 VULN_NAME = "Multiple Headers"
 EXCLUDE_RESPONSE = [200, 301, 302, 403, 404, 307, 308, 303, 429]
 
 logger = configure_logger(__name__)
 
-def verify_cache_poisoning(VULN_TYPE: str, conn: http.client.HTTPConnection | http.client.HTTPSConnection, url: str, payload: str, main_status_code: int, authent: tuple[str, str] | None, host: str) -> None:
+
+def verify_cache_poisoning(
+    VULN_TYPE: str,
+    conn: http.client.HTTPConnection | http.client.HTTPSConnection,
+    url: str,
+    payload: str,
+    main_status_code: int,
+    authent: tuple[str, str] | None,
+    host: str,
+) -> None:
     cb = random.randrange(9999)
     res_status = 0
     try:
         for _ in range(5):
-            conn.putrequest("GET", f"/?CPDoS={cb}") #  discrepancy between conn and req : url endpoint isn't reflected in the putrequest
+            conn.putrequest(
+                "GET", f"/?CPDoS={cb}"
+            )  #  discrepancy between conn and req : url endpoint isn't reflected in the putrequest
             conn.putheader("User-Agent", "xxxx")
 
             if VULN_TYPE == "RDH":
@@ -34,10 +44,9 @@ def verify_cache_poisoning(VULN_TYPE: str, conn: http.client.HTTPConnection | ht
             response = conn.getresponse()
             res_status = response.status
             conn.close()
-        #print(url)
+
         uri = f"{url}?CPDoS={cb}"
-        #print(uri)
-        req =  requests.get(uri, auth=authent, timeout=10)
+        req = requests.get(uri, auth=authent, timeout=10)
         if req.status_code == res_status and res_status != main_status_code:
             reason = f"DIFFERENT STATUS-CODE  {main_status_code} > {response.status}"
             print(
@@ -47,8 +56,14 @@ def verify_cache_poisoning(VULN_TYPE: str, conn: http.client.HTTPConnection | ht
         logger.exception(e)
 
 
-def duplicate_headers(conn: http.client.HTTPConnection | http.client.HTTPSConnection, url: str, mh: str, main_status_code: int, authent: tuple[str, str] | None) -> tuple:
-    #VULN_TYPE = "DH"
+def duplicate_headers(
+    conn: http.client.HTTPConnection | http.client.HTTPSConnection,
+    url: str,
+    mh: str,
+    main_status_code: int,
+    authent: tuple[str, str] | None,
+) -> tuple:
+    """VULN_TYPE =  DH"""
     cb = random.randrange(9999)
 
     try:
@@ -60,8 +75,13 @@ def duplicate_headers(conn: http.client.HTTPConnection | http.client.HTTPSConnec
 
         response = conn.getresponse()
 
-        if response.status != main_status_code and response.status not in EXCLUDE_RESPONSE:
-            #print(f"[{url}?cb={cb}] Statut : {response.status}, Raison : {response.reason}")
+        if (
+            response.status != main_status_code
+            and response.status not in EXCLUDE_RESPONSE
+        ):
+            logger.debug(
+                f"[{url}?cb={cb}] Statut : {response.status}, Raison : {response.reason}"
+            )
             for rh in response.headers:
                 if "age" in rh.lower() or "hit" in rh.lower():
                     return response, cb
@@ -72,10 +92,15 @@ def duplicate_headers(conn: http.client.HTTPConnection | http.client.HTTPSConnec
     finally:
         conn.close()
         return tuple()
-        
 
-def referer_duplicate_headers(conn: http.client.HTTPConnection | http.client.HTTPSConnection, url: str, main_status_code: int, authent: tuple[str, str] | None) -> tuple:
-    #VULN_TYPE = "RDH"
+
+def referer_duplicate_headers(
+    conn: http.client.HTTPConnection | http.client.HTTPSConnection,
+    url: str,
+    main_status_code: int,
+    authent: tuple[str, str] | None,
+) -> tuple:
+    """VULN_TYPE = RDH"""
     cb = random.randrange(9999)
 
     try:
@@ -86,8 +111,13 @@ def referer_duplicate_headers(conn: http.client.HTTPConnection | http.client.HTT
         conn.endheaders()
 
         response = conn.getresponse()
-        if response.status != main_status_code and response.status not in EXCLUDE_RESPONSE:
-            #print(f"[{url}?cb={cb}] Statut : {response.status}, Raison : {response.reason}")
+        if (
+            response.status != main_status_code
+            and response.status not in EXCLUDE_RESPONSE
+        ):
+            logger.debug(
+                f"[{url}?cb={cb}] Statut : {response.status}, Raison : {response.reason}"
+            )
             for rh in response.headers:
                 if "age" in rh.lower() or "hit" in rh.lower():
                     return response, cb
@@ -97,11 +127,16 @@ def referer_duplicate_headers(conn: http.client.HTTPConnection | http.client.HTT
     finally:
         conn.close()
         return tuple()
-        
 
 
-def host_duplicate_headers(conn: http.client.HTTPConnection | http.client.HTTPSConnection, host: str, url: str, main_status_code: int, authent: tuple[str, str] | None) -> tuple:
-    #VULN_TYPE = "HDH"
+def host_duplicate_headers(
+    conn: http.client.HTTPConnection | http.client.HTTPSConnection,
+    host: str,
+    url: str,
+    main_status_code: int,
+    authent: tuple[str, str] | None,
+) -> tuple:
+    """VULN_TYPE = HDH"""
     cb = random.randrange(9999)
 
     try:
@@ -112,8 +147,13 @@ def host_duplicate_headers(conn: http.client.HTTPConnection | http.client.HTTPSC
         conn.endheaders()
 
         response = conn.getresponse()
-        if response.status != main_status_code and response.status not in EXCLUDE_RESPONSE:
-            #print(f"[{url}?cb={cb}] Statut : {response.status}, Raison : {response.reason}")
+        if (
+            response.status != main_status_code
+            and response.status not in EXCLUDE_RESPONSE
+        ):
+            logger.debug(
+                f"[{url}?cb={cb}] Statut : {response.status}, Raison : {response.reason}"
+            )
             for rh in response.headers:
                 if "age" in rh.lower() or "hit" in rh.lower():
                     return response, cb
@@ -123,10 +163,11 @@ def host_duplicate_headers(conn: http.client.HTTPConnection | http.client.HTTPSC
     finally:
         conn.close()
         return tuple()
-        
 
 
-def MHC(url: str, req_main: requests.Response, authent: tuple[str, str] | None, human: str) -> None:
+def MHC(
+    url: str, req_main: requests.Response, authent: tuple[str, str] | None, human: str
+) -> None:
     main_status_code = req_main.status_code
     try:
         parsed_url = urlparse(url)
@@ -146,7 +187,11 @@ def MHC(url: str, req_main: requests.Response, authent: tuple[str, str] | None, 
             vuln_type_res = locals()[vuln_type]
             print(f" {Colors.BLUE} {VULN_NAME} : {url}{Colors.RESET}\r", end="")
             print("\033[K", end="")
-            if vuln_type_res and vuln_type_res is not None and isinstance(vuln_type_res, tuple):
+            if (
+                vuln_type_res
+                and vuln_type_res is not None
+                and isinstance(vuln_type_res, tuple)
+            ):
                 behavior = f"DIFFERENT STATUS-CODE  {main_status_code} > {vuln_type_res[0].status}"
 
                 if vuln_type == "RDH":
@@ -155,12 +200,14 @@ def MHC(url: str, req_main: requests.Response, authent: tuple[str, str] | None, 
                     payload = f"[Host: {host}, Host: toto.com]"
 
                 print(
-                        f" {Identify.behavior} | {VULN_NAME} | {Colors.BLUE}{url}?cb={vuln_type_res[1]}{Colors.RESET} | {behavior} | PAYLOAD: {Colors.THISTLE}{payload}{Colors.RESET}"
-                    )
+                    f" {Identify.behavior} | {VULN_NAME} | {Colors.BLUE}{url}?cb={vuln_type_res[1]}{Colors.RESET} | {behavior} | PAYLOAD: {Colors.THISTLE}{payload}{Colors.RESET}"
+                )
                 conn.close()
-                verify_cache_poisoning(vuln_type, conn, url, payload, main_status_code, authent, host)
+                verify_cache_poisoning(
+                    vuln_type, conn, url, payload, main_status_code, authent, host
+                )
 
-        #m_heads = ["Authorization", "Accept", "Content-Type", "Cookie", "X-Requested-With", "user-agent"]
+        # m_heads = ["Authorization", "Accept", "Content-Type", "Cookie", "X-Requested-With", "user-agent"]
         m_heads = header_list
         for mh in m_heads:
             DH = duplicate_headers(conn, url, mh, main_status_code, authent)
@@ -170,10 +217,12 @@ def MHC(url: str, req_main: requests.Response, authent: tuple[str, str] | None, 
                 payload = f"[{mh}: xxxx, {mh}: xxxx]"
 
                 print(
-                        f" {Identify.behavior} | {VULN_NAME} | {Colors.BLUE}{url}?cb={DH[1]}{Colors.RESET} | {behavior} | PAYLOAD: {Colors.THISTLE}{payload}{Colors.RESET}"
-                    )
+                    f" {Identify.behavior} | {VULN_NAME} | {Colors.BLUE}{url}?cb={DH[1]}{Colors.RESET} | {behavior} | PAYLOAD: {Colors.THISTLE}{payload}{Colors.RESET}"
+                )
                 conn.close()
-                verify_cache_poisoning(mh, conn, url, payload, main_status_code, authent, host)
+                verify_cache_poisoning(
+                    mh, conn, url, payload, main_status_code, authent, host
+                )
             human_time(human)
             print(f" {Colors.BLUE} {VULN_NAME} : {mh}{Colors.RESET}\r", end="")
             print("\033[K", end="")
