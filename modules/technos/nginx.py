0cad1a479c44c0d8b73ffb999019c98c1db00b52
Kharaone.BB <bugbounty@khara.one>
2025-09-20 18:45:28 +0200

    refactor(nginx): enhance code readability by improving logger configuration and exception handling

--- modules/technos/nginx.py
+++ modules/technos/nginx.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
-from utils.utils import requests
+from utils.utils import configure_logger, requests
+
+logger = configure_logger(__name__)
 
 
 def nginx(url: str, s: requests.Session) -> None:
@@ -12,18 +14,22 @@ def nginx(url: str, s: requests.Session) -> None:
     try:
         uqe_url = f'{url}%2F?"><u>plop123</u>'
         uqe_req = s.get(uqe_url, verify=False, timeout=6)
-        if uqe_req not in [403, 401, 400, 500]:
+        if uqe_req.status_code not in [403, 401, 400, 500]:
             if "plop123" in uqe_req.text:
-                #print("coucou")
+                # print("coucou")
                 pass
-    except Exception:
-        pass
+    except Exception as e:
+        logger.exception(f"Error with UQE check on {url}", e)
+
     nginx_headers = [{"X-Real-IP": "plop123"}, {"Forwarded": "plop123"}]
     for ngh in nginx_headers:
         try:
             x_req = s.get(url, headers=ngh, verify=False, timeout=10)
-            print(f"   └── {ngh}{'→':^3} {x_req.status_code:>3} [{len(x_req.content)} bytes]")
+            print(
+                f"   └── {ngh}{'→':^3} {x_req.status_code:>3} [{len(x_req.content)} bytes]"
+            )
             if "plop123" in x_req.text:
                 print("plop123 reflected in text with {ngh} payload")
-        except Exception:
+        except Exception as e:
             print(f"   └── Error with {ngh} payload")
+            logger.exception(f"Error with UQE check on {url}", e)
